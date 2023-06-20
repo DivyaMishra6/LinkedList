3736fd02c56db02bda176daec8c1629283ab052a
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-21 00:27:56 +0530

    [Divya]: Welcome to the LinkedList Program

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@
             Console.WriteLine("Welcome to LinkedList Program");
 
             Console.WriteLine("**********************************");
-           
+            List list = new List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
