84dd9d8d106fb77d33b9040dab2544532d0396dd
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-22 00:57:36 +0530

    [Divya]:Create LinkedList create by Appending

--- Program.cs
+++ Program.cs
@@ -7,10 +7,20 @@
             Console.WriteLine("Welcome to LinkedList Program");
 
             Console.WriteLine("**********************************");
+
+            Console.WriteLine("Welcome to Linkedlist problem!");
+            Console.WriteLine("**********************************");
             List list = new List();
             list.Add(56);
+            Console.WriteLine("Node with data 56 is First Created");
+            Console.WriteLine(" ");
             list.Add(30);
+            Console.WriteLine("Next Append 30 to 56");
+            Console.WriteLine(" ");
             list.Add(70);
+            Console.WriteLine("Finally Append 70 to 30");
+            Console.WriteLine(" ");
+            Console.WriteLine("LinkedList Sequence:");
             list.Display();
         }
     }
